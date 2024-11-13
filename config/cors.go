da35e5276567b842f81e9ac169231f5a99cb1371
Rolly Maulana Awangga <awangga@gmail.com>
2024-11-14 05:34:30 +0700

    add cors

--- config/cors.go
+++ config/cors.go
@@ -6,7 +6,7 @@ import (
 
 // Daftar origins yang diizinkan
 var Origins = []string{
-	"https://www.bukupedia.co.id",
+	"https://petapedia.if.co.id",
 	"https://naskah.bukupedia.co.id",
 	"https://bukupedia.co.id",
 }
